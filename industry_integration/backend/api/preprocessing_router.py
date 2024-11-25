4f107623c6533b72ac4b71e4cbc8e7099d0b3584
jwlee <jwlee@pcninc.co.kr>
2024-11-25 14:25:11 +0900

    create PandasDataFrame class and mofify router add_file_data

--- industry_integration/backend/api/preprocessing_router.py
+++ industry_integration/backend/api/preprocessing_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Request
-from services.dataframe import PolarsDataFrame
+from services.dataframe import PolarsDataFrame, PandasDataFrame
 import duckdb
 
 router = APIRouter()
@@ -14,14 +14,16 @@ async def add_file_data(request: Request):
     table_name = data['table_name']
 
 
-    pf = PolarsDataFrame()
-    paths = pf.get_all_file_paths(abs_path_of_file) # 모든 파일 데이터 경로 추출
-    df = pf.make_polars_dataframe(paths) # 추출한 경로 데이터프레임 생성
-    df_pandas = df.to_pandas()
+    # pf = PolarsDataFrame()
+    pd = PandasDataFrame()
+
+    paths = pd.get_all_file_paths(abs_path_of_file) # 모든 파일 데이터 경로 추출
+    df = pd.make_pandas_dataframe(paths) # 추출한 경로 데이터프레임 생성
+    # df_pandas = df.to_pandas()
     
     con = duckdb.connect(database='./database.db')
     con.execute(f'DROP TABLE IF EXISTS {table_name}')
-    con.execute(f'CREATE TABLE {table_name} AS SELECT * FROM df_pandas')
+    con.execute(f'CREATE TABLE "{table_name}" AS SELECT * FROM "{df}"')
 
     return {'message': '데이터 저장 완료!'}
 
--- industry_integration/backend/services/dataframe.py
+++ industry_integration/backend/services/dataframe.py
@@ -1,5 +1,7 @@
-import glob, os
+import glob
+import os
 import polars as pl
+import pandas as pd
 from concurrent.futures import ThreadPoolExecutor
 
     
@@ -56,6 +58,53 @@ class PolarsDataFrame:
     def make_polars_dataframe(self, paths):
         '''polars_dataframe 생성'''
         df = pl.DataFrame({
+            "full_path": paths,
+            "file_id": self.extract_file_id(paths),
+            "file_name": self.extract_file_name(paths),
+            "folder_name": self.extract_folder_name(paths),
+            "file_size": self.extract_file_size(paths)
+        })
+        return df
+
+class PandasDataFrame:
+    '''
+    polars dataframe 생성
+    '''
+    
+    def __init__(self):
+        pass
+
+
+
+    def get_all_file_paths(self, root_path):
+        # glob를 사용하여 하위 디렉토리까지 검색
+        paths = glob.glob(os.path.join(root_path, '**'), recursive=True)
+        
+        # .zip 파일을 제외한 파일들만 필터링
+        file_paths = [path for path in paths if os.path.isfile(path) and not path.endswith('.zip')]
+        
+        return file_paths
+    
+    def _extract_data(self, paths, extractor):
+        with ThreadPoolExecutor() as executor:
+            return list(executor.map(extractor, paths))
+
+    def extract_file_id(self, paths):
+        # 순차적인 ID 생성 (1, 2, 3, ...)
+        return list(range(1, len(paths) + 1))
+
+    def extract_file_name(self, paths):
+        return self._extract_data(paths, lambda path: os.path.basename(path))
+
+    def extract_folder_name(self, paths):
+        return self._extract_data(paths, lambda path: os.path.basename(os.path.dirname(path)))
+
+    def extract_file_size(self, paths):
+        return self._extract_data(paths, lambda path: os.path.getsize(path))
+
+    def make_pandas_dataframe(self, paths):
+        '''polars_dataframe 생성'''
+        df = pd.DataFrame({
             "full_path": paths,
             "file_id": self.extract_file_id(paths),
             "file_name": self.extract_file_name(paths),
