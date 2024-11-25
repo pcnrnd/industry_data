da3da5a2912199e003f32b62923bc00d153babb1
jwlee <jwlee@pcninc.co.kr>
2024-11-25 13:34:51 +0900

    modify query of read_file_data

--- industry_integration/backend/api/preprocessing_router.py
+++ industry_integration/backend/api/preprocessing_router.py
@@ -18,7 +18,6 @@ async def add_file_data(request: Request):
     paths = pf.get_all_file_paths(abs_path_of_file) # 모든 파일 데이터 경로 추출
     df = pf.make_polars_dataframe(paths) # 추출한 경로 데이터프레임 생성
     
-    # con = duckdb.connect(database=':default:')
     con = duckdb.connect(database='./database.db')
     con.execute(f'DROP TABLE IF EXISTS {table_name}')
     con.execute(f'CREATE TABLE {table_name} AS SELECT * FROM df')
@@ -27,24 +26,15 @@ async def add_file_data(request: Request):
 
 @router.get('/show_tables')
 async def show_tables():
-    # con = duckdb.connect(database=':default:')
     con = duckdb.connect(database='./database.db')
-    con.execute('DROP TABLE IF EXISTS sand_data')
-    con.execute('CREATE TABLE sand_data AS SELECT * FROM df')
-
-    # con = duckdb.connect(database=':dafault:')
     tables = con.execute('SHOW TABLES').fetchall()
     return {'tables': tables}
     
-@router.get('/read_file_data')
-async def read_file_data(request: Request):
+@router.get('/read_file_data/{table_name}')
+def read_file_data(table_name: str):
     '''
     read unstructured data table
     '''
-    data = await request.json()
-    table_name = data['table_name']
-    
-    # con = duckdb.connect(database=':default:')
     con = duckdb.connect(database='./database.db')
     query_result = con.execute(f'SELECT * FROM {table_name}').fetchall()
 
--- industry_integration/front/preprocessing/preprocessing.py
+++ industry_integration/front/preprocessing/preprocessing.py
@@ -9,45 +9,58 @@ import requests, glob, os
 
 st.set_page_config(layout='wide')
 
-with st.form("path_and_tablee_name"):
-    st.write("Input path and tablee name")
+col1, col2 = st.columns(2)
+with col1:
+    with st.form("path_and_tablee_name"):
+        st.write("Input path and tablee name")
 
-    text_data = st.text_input('input data path')
-    table_name = st.text_input('input table name')
+        text_data = st.text_input('input data path')
+        table_name = st.text_input('input table name')
 
-    json_data = {'abs_path': text_data,
-                'table_name': table_name}
+        json_data = {'abs_path': text_data,
+                    'table_name': table_name}
 
-    submitted = st.form_submit_button("Submit")
-    if submitted:
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            try:
+                response = requests.post('http://industry_backend:8000/preprocessing/add_file_data', json=json_data)
+
+                # 응답 처리
+                if response.status_code == 200:
+                    st.success("Data submitted successfully!")
+                    st.json(response.json())  # API 응답 데이터 출력
+                else:
+                    st.error(f"Failed to submit data: {response.status_code}")
+                    st.write(response.text)  # 오류 메시지 출력
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
+
+with col2:
+    with st.form("Show tables"):
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            try:
+                response = requests.get('http://industry_backend:8000/preprocessing/show_tables')
+
+                # 응답 처리
+                if response.status_code == 200:
+                    st.success("Data submitted successfully!")
+                    st.json(response.json())  # API 응답 데이터 출력
+                else:
+                    st.error(f"Failed to submit data: {response.status_code}")
+                    st.write(response.text)  # 오류 메시지 출력
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
+
+    with st.form('select data'):
+        table_name = st.text_input('input table name')
         try:
-            response = requests.post('http://industry_backend:8000/preprocessing/add_file_data', json=json_data)
-
-            # 응답 처리
+            response = requests.get('http://industry_backend:8000/preprocessing/read_file_data?table_name={table_name}', json=json_data)
             if response.status_code == 200:
-                st.success("Data submitted successfully!")
-                st.json(response.json())  # API 응답 데이터 출력
+                st.success('Query success!')
+                st.json(response.json())
             else:
                 st.error(f"Failed to submit data: {response.status_code}")
-                st.write(response.text)  # 오류 메시지 출력
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
-
-
-with st.form("Show tables"):
-    submitted = st.form_submit_button("Submit")
-    if submitted:
-        try:
-            response = requests.get('http://industry_backend:8000/preprocessing/show_tables')
-
-            # 응답 처리
-            if response.status_code == 200:
-                st.success("Data submitted successfully!")
-                st.json(response.json())  # API 응답 데이터 출력
-            else:
-                st.error(f"Failed to submit data: {response.status_code}")
-                st.write(response.text)  # 오류 메시지 출력
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
-
-
+                st.write(response.text)  # 오류 메시지 출력         
+        except: Exception as e:
+            st.error(f'An error occurred: {e}')
